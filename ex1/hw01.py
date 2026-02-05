b3d8fbb9b580f5f4bc1aabe98b36e91b9705413a
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2026-02-04 16:53:00 -0800

    Init hw01

---
+++ ex1/hw01.py
@@ -0,0 +1,9 @@
+# Please correct my errors.
+first_10_bp = "gggtgcgacg'
+second 10_bp = "attcattgtt"
+gene = first_10-bp + second 10_bp
+
+ print('The first 20 bp of BRAC2 gene are gene']
+
+# Expected output:
+# The first 20 bp of BRAC2 gene are gggtgcgacgattcattgtt
