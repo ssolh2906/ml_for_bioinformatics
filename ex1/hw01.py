e5973fe0813b9ec86359d7de762028dbf58798c5
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2026-02-04 16:55:04 -0800

    Correct errors

--- ex1/hw01.py
+++ ex1/hw01.py
@@ -1,9 +1,8 @@
-# Please correct my errors.
-first_10_bp = "gggtgcgacg'
-second 10_bp = "attcattgtt"
-gene = first_10-bp + second 10_bp
+first_10_bp = "gggtgcgacg"
+second_10_bp = "attcattgtt"
+gene = first_10_bp + second_10_bp
 
- print('The first 20 bp of BRAC2 gene are gene']
+print(f'The first 20 bp of BRAC2 gene are {gene}')
 
 # Expected output:
 # The first 20 bp of BRAC2 gene are gggtgcgacgattcattgtt
